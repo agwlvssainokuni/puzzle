a30bb70b019ed8707d54319b93cfd6945dce63ff
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2012-11-17 23:12:06 +0900

    素数判定 * 素数列を生成/表示    * Scalaで遅延評価を使用

---
+++ primenumber/build.sbt
@@ -0,0 +1,5 @@
+name := "primenumber"
+
+version := "1.0.0"
+
+scalaVersion := "2.9.2"
---
+++ primenumber/src/main/scala/Prime.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 素数列を生成/表示する。
+ */
+object Prime {
+
+	def main(args: Array[String]) =
+		primes take args(0).toInt foreach println
+
+	lazy val primes: Stream[Long] = 2L #:: 3L #:: 5L #:: generate(5L)
+
+	private def generate(n: Long): Stream[Long] = {
+		val p = next(n + 2, primes)
+		p #:: generate(p)
+	}
+
+	private def next(n: Long, p: Stream[Long]): Long =
+		if (p.head * p.head > n)
+			n
+		else if (n % p.head == 0)
+			next(n + 2, primes)
+		else
+			next(n, p.tail)
+
+}
