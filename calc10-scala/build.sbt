f620d636d76a0f082f81955a61cd0f5c3992416d
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-02-23 13:36:26 +0900

    10計算(Scala)

--- calc10-scala/build.sbt
+++ calc10-scala/build.sbt
@@ -6,7 +6,7 @@ scalaVersion := "2.10.3"
 
 libraryDependencies ++= Seq(
 	"org.apache.commons" % "commons-lang3" % "3.2.1",
-	"org.slf4j" % "slf4j-api" % "1.7.5",
+	"com.typesafe" %% "scalalogging-slf4j" % "1.0.1",
 	"ch.qos.logback" % "logback-core" % "1.0.13",
 	"ch.qos.logback" % "logback-classic" % "1.0.13"
 )
--- calc10-scala/src/main/scala/cherry/calc10/Calc10.scala
+++ calc10-scala/src/main/scala/cherry/calc10/Calc10.scala
@@ -1,28 +1,49 @@
+/*
+ * Copyright 2006,2014 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
 package cherry.calc10
 
 import org.apache.commons.lang3.math.Fraction
 
-object Calc10 extends Application {
+import com.typesafe.scalalogging.slf4j.Logging
+
+object Calc10 extends App with Logging {
 
   val TEN = Fraction.getFraction(10, 1)
 
   for {
-    number <- 0 to 9999
-    n0 = node(number % 10); div0 = number / 10
-    n1 = node(div0 % 10); div1 = div0 / 10
-    n2 = node(div1 % 10); div2 = div1 / 10
-    n3 = node(div2 % 10)
-    result = (for {
-      node <- combinations(Seq(n3, n2, n1, n0))
-      if node.value.equals(TEN)
-    } yield {
-      node.expression
-    }).distinct
+    num <- 0 to args(0).toInt
+    l = num % 10; div0 = num / 10
+    k = div0 % 10; div1 = div0 / 10
+    j = div1 % 10; div2 = div1 / 10
+    i = div2 % 10
   } {
-    printf("%d: %s\n", number, result.mkString(","))
+    (for {
+      node <- combinations(Seq(node(i), node(j), node(k), node(l)))
+      if node.value.equals(TEN)
+    } yield node.expression).distinct match {
+      case result =>
+        logger.info(Seq(
+          num,
+          result.size,
+          result.mkString("\"", ",", "\"")).mkString(","))
+    }
   }
 
-  def node(n: Int) = NumberNode(Fraction.getFraction(n, 1))
+  def node(n: Int) = NumNode(Fraction.getFraction(n, 1))
 
   def combinations(nodes: Seq[Node]): Seq[Node] =
     if (nodes.size <= 1)
@@ -35,16 +56,15 @@ object Calc10 extends Application {
         op2 <- combinations(nodes.diff(comb))
       } yield {
         Seq(
-          AdditionNode(op1, op2),
-          MultiplicationNode(op1, op2),
-          SubtractionNode(op1, op2),
-          SubtractionNode(op2, op1)) ++
+          AddNode(op1, op2),
+          MulNode(op1, op2),
+          SubNode(op1, op2),
+          SubNode(op2, op1)) ++
           (if (!op2.isZero)
-            Seq(DivisionNode(op1, op2))
-          else
-            Seq()) ++
+            Seq(DivNode(op1, op2))
+          else Seq()) ++
           (if (!op1.isZero)
-            Seq(DivisionNode(op2, op1))
+            Seq(DivNode(op2, op1))
           else Seq())
       }).flatten
 
@@ -56,33 +76,33 @@ trait Node {
   def isZero = value.equals(Fraction.ZERO)
 }
 
-trait OperationNode extends Node {
+trait OpNode extends Node {
   val operator: String
   lazy val expression = "(" + op1.expression + operator + op2.expression + ")"
   val op1: Node
   val op2: Node
 }
 
-case class NumberNode(val value: Fraction) extends Node {
+case class NumNode(val value: Fraction) extends Node {
   lazy val expression: String = value.intValue.toString
 }
 
-case class AdditionNode(val op1: Node, val op2: Node) extends OperationNode {
+case class AddNode(val op1: Node, val op2: Node) extends OpNode {
   val operator = "+"
   lazy val value = op1.value.add(op2.value)
 }
 
-case class SubtractionNode(val op1: Node, val op2: Node) extends OperationNode {
+case class SubNode(val op1: Node, val op2: Node) extends OpNode {
   val operator = "-"
   lazy val value = op1.value.subtract(op2.value)
 }
 
-case class MultiplicationNode(val op1: Node, val op2: Node) extends OperationNode {
+case class MulNode(val op1: Node, val op2: Node) extends OpNode {
   val operator = "*"
   lazy val value = op1.value.multiplyBy(op2.value)
 }
 
-case class DivisionNode(val op1: Node, val op2: Node) extends OperationNode {
+case class DivNode(val op1: Node, val op2: Node) extends OpNode {
   val operator = "/"
   lazy val value = op1.value.divideBy(op2.value)
 }
